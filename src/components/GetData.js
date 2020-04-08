a2395a568daf2e571789e6c768211d88f3955178
vishalicious213 <vishalicious213@gmail.com>
2020-04-08 01:12:23 -0400

    moved Calendar component into GetData so I could use the date in state. Added date to API call. it works! It renders the page based on the date clicked!

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -2,24 +2,41 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ShowPicture from './Picture';
 import ShowData from './Data';
-import Calendar from './Calendar';
+// import Calendar from './Calendar';
 import './GetData.css';
 
 function GetData() {
+    // const today = new Date();
     const [nasaData, setNasaData] = useState();
+    const [date, setDate] = useState('');
+
+    // console.log(date);
 
     useEffect(() => {
         const fetchData = () => {
             axios
                 // .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-                .get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`)
+                .get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=${date}`)
                 .then(response => setNasaData(response.data))
                 // .then(console.log(nasaData))
                 .catch(error => console.log(error))
         }
 
         fetchData();
-    }, []);
+    }, [date]);
+
+    const Calendar = () => {
+        function getDate(event) {
+            console.log(event.target.value);
+            setDate(event.target.value);
+        }
+
+        return (
+            <div>
+                <input type='date' onChange={getDate}/>
+            </div>
+        )
+    }
 
     if (!nasaData) return <h3>Loading...</h3>;
     // console.log('API KEY: ', process.env.REACT_APP_API_KEY);
